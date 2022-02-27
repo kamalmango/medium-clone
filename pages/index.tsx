036085e94c73ec17c90e007d7d805cc22b6ce325
Kamal Mango <kamal.mango7@gmail.com>
2022-02-27 19:35:00 +0100

    implement posts section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Header from "../components/Header"
+import Link from "next/link"
 import { sanityClient, urlFor } from "../sanity"
 import { Post } from "../typings"
 
@@ -39,6 +40,32 @@ export default function Home({ posts }: Props) {
       </div>
 
       {/* Posts */}
+      <div className="lg:gird-cols-3 grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="eas-in-out h-60 w-full object-cover transition-transform duration-200 group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p className="text-lg font-bold">{post.title}</p>
+                  <p className="text-xs">
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()!}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
